7cb445544ac8d32ac834a1dd9bcfcf2dfb4d78e7
mgk <mgk@gmail.com>
2024-06-17 12:34:49 +0530

    qa

---
+++ src/test/java/git/Demo4.java
@@ -0,0 +1,30 @@
+package git;
+
+import org.testng.annotations.Test;
+
+public class Demo4 {
+
+	@Test
+	public void demo1()
+	{
+		System.out.println();
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println();
+	}
+	
+	@Test
+	public void demo3()
+	{
+		System.out.println();
+	}
+	
+	@Test
+	public void demo4()
+	{
+		System.out.println();
+	}
+}
